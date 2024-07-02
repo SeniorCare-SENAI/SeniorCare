c1bf359f0b07f349c9daa004751bb4583e428df7
Bryan Prinz <146386080+Bryanps7@users.noreply.github.com>
2024-07-02 17:39:26 -0300

    Quase acabando essa merda

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -7,3 +7,16 @@ function select(section) {
         }
     }
 }
+
+function label() {
+    let labeis = document.getElementsByClassName('labeis')
+
+    labeis.style.transform = "translateY(-31px)"
+    labeis.style.fontSize = "12px"
+    labeis.style.color = "var(--verde-pouco-escuro)"
+    labeis.style.backgroundColor = "white"
+    labeis.style.borderRadius = "var(--border-radius) var(--border-radius) 0 0"
+    labeis.style.border = "var(--verde-escuro) 0.5px solid"
+    labeis.style.padding = "0 var(--padding)"
+    labeis.style.borderBottom = "0"
+}

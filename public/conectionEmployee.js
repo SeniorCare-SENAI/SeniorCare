015a37f2610f6d6ea86cd8ebf6c9b7494d3759d5
Vinicius_ruas <146386108+ViniB22@users.noreply.github.com>
2024-07-09 13:42:43 -0300

    b

--- public/conectionEmployee.js
+++ public/conectionEmployee.js
@@ -33,12 +33,53 @@ cadastrarFuncionario.addEventListener('click', ()=>{
         console.error("Erro de conexão",err)
     })
 })
-buscarFuncionario.addEventListener('click',()=>{
-    const codFunc = document.getElementById('codFunc').value
 
+listar.addEventListener('click', ()=>{
+    fetch('http://localhost:8080/funcionario')
+    .then(resposta => resposta.json())
+    .then(dados => {
+        resList.innerHTML = "";
+        dados.forEach(dado => {
+            res.innerHTML += "Código: " + dado.codFuncionario + "<br>" 
+            + "Nome: " + dado.nomeFuncionario + "<br>" 
+            + "Cargo: " + dado.cargoFuncionario + "<br>" 
+            + "CPF: " + dado.cpfFuncionario  +  "<br>"  
+            + "Situacao: " + dado.situacaoFuncionario + "<br>" 
+            + "Telefone: " + dado.telefoneFuncionario 
+        })
+    })
+    .catch((err) => console.error("Falha ao listar todos os produtos", err))
+});
 
-})
+buscarFuncionario.addEventListener('click', ()=>{
+    let codFunc = Number(document.getElementById('codFunc').value)
 
+    fetch(`http://localhost:8080/produto/${codFunc}`)
+    .then(resposta => resposta.json())
+    .then(dados => {
+        res.innerHTML = "Código: " + dados.codFuncionario + "<br>" 
+        + "Nome: " + dados.nomeFuncionario + "<br>" 
+        + "Cargo: " + dados.cargoFuncionario  + "<br>" 
+        + "CPF: " + dados.cpfFuncionario + "<br>" 
+        + "Situacao: " + dados.situacaoFuncionario + "<br>"
+        + "Telefone: " + dados.telefoneFuncionario 
+    })
+    .catch((err) => console.error("Falha ao tentar encontrar produto!", err))
+});
+
+deletar.addEventListener('click', ()=>{
+    let codProduto = Number(document.getElementById('codProduto').value)
+
+    fetch(`http://localhost:8080/produto/${codProduto}`,{
+        method: "DELETE",
+        headers: {"Content-Type":"application/json"}
+    })
+    .then(resposta => resposta.text())
+    .then(dados => {
+        res.innerHTML = "produto apagado com sucesso!"
+    })
+    .catch((err) => console.error("Erro ao apagar o produto!", err))
+});
 function escolha() {
     let escolha = confirm('Tem certeza que desejá apagar?')
 

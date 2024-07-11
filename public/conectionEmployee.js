13695dff9275ce6014a5b97c4aa26c699a46446b
Vinicius_ruas <146386108+ViniB22@users.noreply.github.com>
2024-07-11 13:44:02 -0300

    outras atualizações

--- public/conectionEmployee.js
+++ public/conectionEmployee.js
@@ -1,4 +1,12 @@
 let cadastrarFuncionario = document.getElementById('cadastrarFuncionario')
+let buscarFuncionario = document.getElementById('buscarFuncionario')
+let apagarFuncionario = document.getElementById('apagarFuncionario')
+let listarFun = document.getElementById('listarFun')
+
+let resposta = document.getElementById('resposta')
+let resproc = document.getElementById('resproc')
+let resapaga = document.getElementById('resapaga')
+let res = document.getElementById('res')
 
 cadastrarFuncionario.addEventListener('click', ()=>{
     const nomeFunc = document.getElementById('nomeFunc').value
@@ -22,9 +30,9 @@ cadastrarFuncionario.addEventListener('click', ()=>{
         resposta.innerHTML = `você não pode deixar de colocar as informações Nome Funcionário, CPF, Telefone ou cargo`
     }else{
         fetch('http://localhost:8080/funcionario',{
-        method : "POST",
-        headers : {"Content-Type":"application/json"},
-        body: JSON.stringify(dados)
+            method : "POST",
+            headers : {"Content-Type":"application/json"},
+            body: JSON.stringify(dados)
         })
         .then(resultado => resultado.json())
         .then(valores => {
@@ -42,64 +50,62 @@ cadastrarFuncionario.addEventListener('click', ()=>{
     }    
 })
 
-// listarFun.addEventListener('click', ()=>{
-//     fetch('http://localhost:8080/funcionario')
-//     .then(resposta => resposta.json())
-//     .then(dados => {
-//         resList.innerHTML = "";
-//         dados.forEach(dado => {
-//             res.innerHTML += "Código: " + dado.codFuncionario + "<br>" 
-//             + "Nome: " + dado.nomeFuncionario + "<br>" 
-//             + "Cargo: " + dado.cargoFuncionario + "<br>" 
-//             + "CPF: " + dado.cpfFuncionario  +  "<br>"  
-//             + "Situacao: " + dado.situacaoFuncionario + "<br>" 
-//             + "Telefone: " + dado.telefoneFuncionario 
-//         })
-//     })
-//     .catch((err) => console.error("Falha ao listar todos os produtos", err))
-// });
 
-// buscarFuncionario.addEventListener('click', ()=>{
-//     let codFunc = Number(document.getElementById('codFunc').value)
+buscarFuncionario.addEventListener('click', ()=>{
+    let codFunc = Number(document.getElementById('codFunc').value)
+    
+    const dados = {
+        codFuncionario: codFunc
+    }
+    
+    fetch(`http://localhost:8080/funcionario/${codFunc}`)
+    .then(resposta => resposta.json(dados))
+    .then(valores => {
+        resproc.innerHTML = "Código: " + valores.codFuncionario + "<br>" 
+        + "Nome: " + valores.nomeFuncionario + "<br>" 
+        + "Cargo: " + valores.cargoFuncionario  + "<br>" 
+        + "CPF: " + valores.cpfFuncionario + "<br>" 
+        + "Situacao: " + valores.situacaoFuncionario + "<br>"
+        + "Telefone: " + valores.telefoneFuncionario 
+    })
+    .catch((err) => console.error("Falha ao tentar encontrar funcionário!", err))
+});
 
-//     const dados = {
-//         codFuncionario: nomeFunc
-//     }
-
-//     fetch(`http://localhost:8080/funcionario/${codFunc}`,{
-//         method : "GET",
-//         headers : {"Content-Type":"application/json"},
-//         body: JSON.stringify(dados)
-//     })
-//     .then(resposta => resposta.json())
-//     .then(dados => {
-//         resproc.innerHTML = "Código: " + dados.codFuncionario + "<br>" 
-//         + "Nome: " + dados.nomeFuncionario + "<br>" 
-//         + "Cargo: " + dados.cargoFuncionario  + "<br>" 
-//         + "CPF: " + dados.cpfFuncionario + "<br>" 
-//         + "Situacao: " + dados.situacaoFuncionario + "<br>"
-//         + "Telefone: " + dados.telefoneFuncionario 
-//     })
-//     .catch((err) => console.error("Falha ao tentar encontrar funcionário!", err))
-// });
-
-// apagarFuncionario.addEventListener('click', ()=>{
-//     let codFunc = Number(document.getElementById('codFunc').value)
-
-//     fetch(`http://localhost:8080/produto/${codFunc}`,{
-//         method: "DELETE",
-//         headers: {"Content-Type":"application/json"}
-//     })
-//     .then(resposta => resposta.text())
-//     .then(dados => {
-//         res.innerHTML = "produto apagado com sucesso!"
-//     })
-//     .catch((err) => console.error("Erro ao apagar o produto!", err))
-// });
-// function escolha() {
-//     let escolha = confirm('Tem certeza que desejá apagar?')
-
-//     if(escolha) {
+apagarFuncionario.addEventListener('click', ()=>{
+    let escolha = confirm('Tem certeza que desejá apagar?')
+    
+    if(escolha) {
+        let codFuncA = Number(document.getElementById('codFuncA').value)
+        console.log(codFuncA)
         
-//     }
-// }
+        fetch(`http://localhost:8080/funcionario/${codFuncA}`,{
+            method: "DELETE",
+            headers: {"Content-Type":"application/json"}
+        })
+            .then(resposta => resposta.text())
+            .then(dados => {
+            resapaga.innerHTML = "Funcionario apagado com sucesso!"
+        })
+        .catch((err) => console.error("Erro ao apagar o funcionario!", err))
+    }else {
+        alert('Digite o código corretamente')
+    }
+    
+});
+
+listarFun.addEventListener('click', ()=>{
+    fetch('http://localhost:8080/funcionario')
+    .then(resposta => resposta.json())
+    .then(dados => {
+        console.log(dados)
+        res.innerHTML = `<table>
+        </table>` 
+
+        res.innerHTML = ""
+        dados.forEach(dad => {
+            res.innerHTML += `<tr> <td> ${dad.codFuncionario} </td> <td> ${dad.nomeFuncionario} </td> <td> ${dad.cargoFuncionario} </td> <td> ${dad.cpfFuncionario} </td> <td> ${dad.situacaoFuncionario} </td> <td> ${dad.telefoneFuncionario} </td> </tr>`
+
+        })
+    })
+    .catch((err) => console.error("Falha ao listar todos os funcionários", err))
+});
